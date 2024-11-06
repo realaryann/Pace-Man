65a74b7d056e02a55586ab89d8fc18a7343d1b54
aryan <arcnagpal1234@gmail.com>
2024-11-05 22:35:26 -0500

    orange pushes

---
+++ Implementation/orange_ghost.cpp
@@ -0,0 +1,152 @@
+#include "orange_ghost.h"
+
+Object_type Orange::who() {
+    return ORANGE_GHOST;
+}
+
+
+Coord Orange::exit_house()
+{
+    swap(world[coord.y][coord.x], world[5][8]);
+    coord.y = 5;
+    coord.x = 8;
+    exithouse = true;
+    return coord;
+}
+
+Coord Orange::move(Coord player) {
+    playerloc = player;
+    Coord target;
+    target.x = playerloc.x+1 - coord.x;
+    target.y = playerloc.y+1 - coord.y;
+    if (target.x > 0) {
+        //try to Move right
+        if ((coord.x + 1 < BOARD_WIDTH) && (world[coord.y][coord.x + 1]->who() != WALL)) {
+            if ((world[coord.y][coord.x + 1]->who() == PLAYER))
+            {
+                world[coord.y][coord.x + 1]->set_lives(world[coord.y][coord.x + 1]->get_lives() - 1);
+                return coord;
+            }
+            swap(world[coord.y][coord.x], world[coord.y][coord.x + 1]);
+            coord.x++;
+            return coord;
+        }
+    }
+    if (target.x < 0) {
+        // try to Move left
+        if ((coord.x > 0) && (world[coord.y][coord.x - 1]->who() != WALL)) {
+            if ((world[coord.y][coord.x - 1]->who() == PLAYER))
+            {
+                world[coord.y][coord.x - 1]->set_lives(world[coord.y][coord.x - 1]->get_lives() - 1);
+                return coord;
+            }
+            swap(world[coord.y][coord.x], world[coord.y][coord.x - 1]);
+            coord.x--;
+            return coord;
+        }
+    }
+    if (target.y < 0) {
+        // try to move up
+        if ((coord.y > 0) && (world[coord.y - 1][coord.x]->who() != WALL)) {
+            if ((world[coord.y - 1][coord.x]->who() == PLAYER))
+            {
+                world[coord.y - 1][coord.x]->set_lives(world[coord.y - 1][coord.x]->get_lives() - 1);
+                return coord;
+            }
+            swap(world[coord.y][coord.x], world[coord.y - 1][coord.x]);
+            coord.y--;
+            return coord;
+        }
+    }
+    if (target.y > 0) {
+        // try to move down
+        if ((coord.y + 1 < BOARD_HEIGHT) && (world[coord.y + 1][coord.x]->who() != WALL)) {
+            if ((world[coord.y + 1][coord.x]->who() == PLAYER))
+            {
+                world[coord.y + 1][coord.x]->set_lives(world[coord.y + 1][coord.x]->get_lives() - 1);
+                return coord;
+            }
+            swap(world[coord.y][coord.x], world[coord.y + 1][coord.x]);
+            coord.y++;
+            return coord;
+        }
+
+    }
+    // Choose a random direction
+    srand(time(NULL));
+    int choice = rand() % 5;
+    switch (choice)
+    {
+    case UP:
+        if ((coord.y > 0) && (world[coord.y - 1][coord.x]->who() != WALL))
+        {
+            dir = UP;
+            if ((world[coord.y - 1][coord.x]->who() == PLAYER))
+            {
+                world[coord.y - 1][coord.x]->set_lives(world[coord.y - 1][coord.x]->get_lives() - 1);
+                return coord;
+            }
+            swap(world[coord.y][coord.x], world[coord.y - 1][coord.x]);
+            coord.y--;
+        }
+        break;
+    case LEFT:
+        if ((coord.x > 0) && (world[coord.y][coord.x - 1]->who() != WALL))
+        {
+            dir = LEFT;
+            if ((world[coord.y][coord.x - 1]->who() == PLAYER))
+            {
+                world[coord.y][coord.x - 1]->set_lives(world[coord.y][coord.x - 1]->get_lives() - 1);
+                return coord;
+            }
+            swap(world[coord.y][coord.x], world[coord.y][coord.x - 1]);
+            coord.x--;
+
+        }
+        break;
+    case RIGHT:
+        if ((coord.x + 1 < BOARD_WIDTH) && (world[coord.y][coord.x + 1]->who() != WALL))
+        {
+            dir = RIGHT;
+            if ((world[coord.y][coord.x + 1]->who() == PLAYER))
+            {
+                world[coord.y][coord.x + 1]->set_lives(world[coord.y][coord.x + 1]->get_lives() - 1);
+                return coord;
+            }
+            swap(world[coord.y][coord.x], world[coord.y][coord.x + 1]);
+            coord.x++;
+        }
+        break;
+    case DOWN:
+        if ((coord.y + 1 < BOARD_HEIGHT) && (world[coord.y + 1][coord.x]->who() != WALL))
+        {
+            dir = DOWN;
+            if ((world[coord.y + 1][coord.x]->who() == PLAYER))
+            {
+                world[coord.y + 1][coord.x]->set_lives(world[coord.y + 1][coord.x]->get_lives() - 1);
+                return coord;
+            }
+            swap(world[coord.y][coord.x], world[coord.y + 1][coord.x]);
+            coord.y++;
+        }
+        break;
+    }
+    return coord;
+
+}
+
+void Orange::display(sf::RenderWindow& window) {
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
+    sprite.setScale(0.06, 0.06);
+    sprite.setOrigin((sf::Vector2f)texture.getSize() / 2.0f);
+    sf::Vector2f display_position;
+    display_position.x = coord.x * 21.7;
+    display_position.y = coord.y * 20.6;
+    sprite.setPosition(display_position);
+    if (dir == LEFT)
+    {
+        sprite.setScale(-0.06f, 0.06f);
+    }
+    window.draw(sprite);
+}
